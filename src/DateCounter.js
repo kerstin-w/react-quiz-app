6e0896706be51fb15d5ebbe0bbc70dbc512a59e9
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-05-03 15:05:06 +0200

    refactor: Change the DateCounter functionallity to useReducer. Instead of updating the DateCounter with useState that function was refactor to useReducer

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,34 +1,76 @@
-import { useState } from "react";
+import { useReducer } from "react";
 
+const initialState = { count: 0, step: 1 };
+
+/**
+ * The reducer function takes in the current state and an action, then updates the state based on the
+ * action type and payload.
+ * @param state - The `state` parameter in the `reducer` function represents the current state of the
+ * application. It contains the data that the application needs to keep track of, such as the count and
+ * step values in this case. The state is updated based on the actions dispatched to the reducer
+ * function.
+ * @param action - The `action` parameter in the `reducer` function is an object that represents the
+ * action being dispatched. It typically has a `type` property that describes the type of action being
+ * performed, and may also contain a `payload` property with additional data related to the action. The
+ * reducer function uses
+ * @returns The reducer function is returning a new state object based on the action type provided. The
+ * state object is being updated based on the action type, such as incrementing or decrementing the
+ * count, setting a new count value, setting a new step value, resetting the state to initial state, or
+ * throwing an error for unknown actions.
+ */
+function reducer(state, action) {
+  console.log(state, action);
+
+  switch (action.type) {
+    case "inc":
+      return { ...state, count: state.count + state.step };
+    case "dec":
+      return { ...state, count: state.count - state.step };
+    case "setCount":
+      return { ...state, count: action.payload };
+    case "setStep":
+      return { ...state, step: action.payload };
+    case "reset":
+      return initialState;
+    default:
+      throw new Error("Unknown action");
+  }
+}
+
+/**
+ * The `DateCounter` function creates a component that allows users to increment or decrement
+ * a count and display the resulting date.
+ * @returns The `DateCounter` component is being returned. It is a functional component that displays a
+ * counter with buttons to increment and decrement the count, a range input to set the step value, and
+ * a button to reset the count. The component also displays a date that is calculated based on the
+ * count value.
+ */
 function DateCounter() {
-  const [count, setCount] = useState(0);
-  const [step, setStep] = useState(1);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, step } = state;
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
 
   const dec = function () {
-    // setCount((count) => count - 1);
-    setCount((count) => count - step);
+    dispatch({ type: "dec" });
   };
 
   const inc = function () {
-    // setCount((count) => count + 1);
-    setCount((count) => count + step);
+    dispatch({ type: "inc" });
   };
 
   const defineCount = function (e) {
-    setCount(Number(e.target.value));
+    dispatch({ type: "setCount", payload: Number(e.target.value) });
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    dispatch({ type: "setStep", payload: Number(e.target.value) });
   };
 
   const reset = function () {
-    setCount(0);
-    setStep(1);
+    dispatch({ type: "reset" });
   };
 
   return (
