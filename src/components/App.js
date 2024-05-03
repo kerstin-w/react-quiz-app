ecb2c5e98d30c9f9a63e586e39ee3a37c74c53b5
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-05-03 17:00:15 +0200

    feat: Add new answer state and a newAnswer case for answer selection functionality. Implemented a new action type newAnswer in the reducer to store user-selected answers. When triggered, this action updates the state with the selected answer payload. In the main component, conditionally rendered the Question component with the Options component to enable users to select answers when the quiz status is active. The Options component now receives additional props: dispatch function to update the state, and answer to track the user's selected answer. Refactored the Options component to handle answer selection functionality. Each option button is now dynamically styled based on whether it is the correct answer, the user's selected answer, or an answer that has already been selected. The buttons are disabled once the user has made a selection to prevent multiple answers.

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,7 @@ const initialState = {
   // loading, error, ready, acttive, finished
   status: "loading",
   index: 0,
+  answer: null,
 };
 
 function reducer(state, action) {
@@ -23,16 +24,17 @@ function reducer(state, action) {
         ...state,
         status: "error",
       };
-
     case "start":
       return { ...state, status: "active" };
+    case "newAnswer":
+      return { ...state, answer: action.payload };
     default:
       throw new Error("Invalid action type");
   }
 }
 
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -55,7 +57,13 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question question={questions[index]} />}
+        {status === "active" && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
