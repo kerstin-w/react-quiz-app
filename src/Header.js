9ee817d43bdd8f375717e98381b58d67894babe1
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-05-03 15:18:35 +0200

    feat: Add Header Component to App.js

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,12 @@
+/**
+ * The Header function returns a header element with an image and a heading for "The React Quiz".
+ * @returns The Header component is being returned, which consists of a header element with an image
+ * displaying the React logo and a heading "The React Quiz".
+ */
 function Header() {
   return (
-    <header className='app-header'>
-      <img src='logo512.png' alt='React logo' />
+    <header className="app-header">
+      <img src="logo512.png" alt="React logo" />
       <h1>The React Quiz</h1>
     </header>
   );
